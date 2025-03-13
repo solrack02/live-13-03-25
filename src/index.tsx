672b693580ad8f3d8464410b88093eaaa68d68a5
Carlos A. <carlosvsk@gmail.com>
2025-03-13 15:41:08 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@
           import { tools } from './tools';
 
           // ---------- set Caps Inputs
-          const currRoute = 'a0';
+          const currRoute = 'sc1';
 
           let args: any = [];
 
